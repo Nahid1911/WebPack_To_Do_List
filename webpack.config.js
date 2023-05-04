10b3fd0ab4077595afe0167898b4813024f87d61
Nahid1911 <nahid.raihan@gmail.com>
2023-05-05 03:40:58 +0600

    webpack environment setup

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    }],
+
+  },
+  plugins: [
+    new ESLintPlugin({
+      files: './src/index.js',
+      extensions: ['js'],
+
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+
+};
