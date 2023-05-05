31dd8c69873f7926c18a694415acf3612e0ea585
Nahid1911 <nahid.raihan@gmail.com>
2023-05-05 16:09:57 +0600

    dynamic functionality added

--- src/index.js
+++ src/index.js
@@ -6,27 +6,15 @@ const tasks = [{ description: 'wash the dishes', completed: false, index: 0 },
   { description: 'complete To Do list project', completed: false, index: 1 }];
 
 const target = document.getElementById('listContainer');
-target.innerHTML = `
+for (let i = 0; i < tasks.length; i += 1) {
+  target.innerHTML += `
 <li class ="commonClass">
 <label class ="checkBoxLabel">
-<input for ="job" id="inputField" type="checkbox" class ="checkBox">
-<p id ="job" class ="pInsideLi">${tasks[0].description}</p>
+<input for ="job${tasks[i].index}" id="inputField" type="checkbox" class ="checkBox">
+<p id ="job${tasks[i].index}" class ="pInsideLi">${tasks[i].description}</p>
 </label>
 <button  class="btnAll threeDots">
  <i class="fa fa-ellipsis-h"></i>
 </button>
-</li>
-<li class ="commonClass">
-<label class ="checkBoxLabel">
-<input for ="job2" id="inputField" type="checkbox" class ="checkBox">
-<p id ="job2" class ="pInsideLi">${tasks[1].description}</p>
-</label>
-<button  class="btnAll threeDots">
- <i class="fa fa-ellipsis-h"></i>
-</button>
-</li>
-
-<div class="commonClass clearBtnDiv" >
-<button class="btnCompleteAll"><p>Clear all completed</p></button>
-</div>
 `;
+}
