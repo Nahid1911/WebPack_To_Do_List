9596a74bdfb514fa1412fa68921de98b760345b9
Nahid1911 <nahid.raihan@gmail.com>
2023-05-05 11:42:34 +0600

    additional functionality removed as revewer recommed

--- src/index.js
+++ src/index.js
@@ -2,50 +2,46 @@
 import _ from 'lodash';
 import './style.css';
 
-const inputField = document.getElementById('inputField');
-function saveToLocalStorage(data) {
-  let tasks = [];
-  if (localStorage.getItem('tasks')) {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
+const tasks = [{ description: 'wash the dishes', completed: false, index: 0 },
+  { description: 'complete To Do list project', completed: false, index: 1 }];
 
-  tasks.push(data);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+const target = document.getElementById('mainContainer');
 
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-const target = document.getElementById('listContainer');
+target.innerHTML = `
+<div class="commonClass">
+<p>Today's To Do</p>
+<button class="btnAll">
+  <i class="fas fa-sync-alt"></i>
+</button>
 
-function displaytasks(task, index) {
-  const content = `
-      <li class ="commonClass">
-      <label class ="checkBoxLabel">
-      <input for ="job${index}" id="inputField" type="checkbox" class ="checkBox">
-      <p id ="job${index}" class ="pInsideLi">${task.description}</p>
-      </label>
-      <button  class="btnAll threeDots">
-       <i class="fa fa-ellipsis-h"></i>
-      </button>
-      </li>
-    `;
-  return content;
-}
+</div>
+<div class="commonClass">
+<input id="inputField" type="text" class="textInput" placeholder="Add to your list..."/>
+<button  class="btnAll threeDots">
+  <i class="fa fa-ellipsis-h"></i>
+</button>
+</div>
+<ul id="listContainer">
+<li class ="commonClass">
+<label class ="checkBoxLabel">
+<input for ="job" id="inputField" type="checkbox" class ="checkBox">
+<p id ="job" class ="pInsideLi">${tasks[0].description}</p>
+</label>
+<button  class="btnAll threeDots">
+ <i class="fa fa-ellipsis-h"></i>
+</button>
+</li>
+<li class ="commonClass">
+<label class ="checkBoxLabel">
+<input for ="job2" id="inputField" type="checkbox" class ="checkBox">
+<p id ="job2" class ="pInsideLi">${tasks[1].description}</p>
+</label>
+<button  class="btnAll threeDots">
+ <i class="fa fa-ellipsis-h"></i>
+</button>
+</ul>
 
-for (let i = 0; i < tasks.length; i += 1) {
-  const task = displaytasks(tasks[i], i);
-  target.innerHTML += task;
-}
-
-inputField.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    const inputValue = inputField.value;
-    const newObj = {
-      description: inputValue,
-      completed: false,
-      index: null,
-    };
-    saveToLocalStorage(newObj);
-    window.location.reload();
-    inputField.value = '';
-  }
-});
+<div class="commonClass clearBtnDiv" >
+<button class="btnCompleteAll"><p>Clear all completed</p></button>
+</div>
+`;
