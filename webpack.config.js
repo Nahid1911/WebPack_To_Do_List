d2bf5073700748b23788f41b68e128c9ad85a152
Nahid1911 <nahid.raihan@gmail.com>
2023-05-09 12:16:48 +0600

    add remove and add todo item function

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,17 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ],
   },
 
